f3f9d6fbc98893c550c8f055969a8aabf2567b34
John Phillips <madhatter003@yahoo.com>
2019-05-28 13:20:12 -0600

    created searchbar and recentposts components and used in app component

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 
-
 class Logo extends Component {
-    render() {
-        return (
-            <div className="logo-main">
-                <img alt="Photo goes here" src="/assets/DSlogo.png"/>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="logo-main-wrapper">
+				<div className="logo-main">
+					<img alt="Photo goes here" src="/assets/DSlogo.png" />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Logo;
