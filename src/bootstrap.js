60a353ab1f94d29072eba20eeefa9c860915a2ca
John Phillips <madhatter003@yahoo.com>
2019-05-29 10:41:09 -0600

    added exact route for home and setup HOC withRouter to navigate to results router

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,20 +3,24 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "./components/home";
+
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+// import 'bootstrap/dist/css/bootstrap.css';
 import "./style/main.scss";
+
+import Home from "./components/home";
+import Results from './components/results';
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <Switch>
-          <Route path='/' component={Home}/>
-          <Route path ='/results' component={Home}/>
+          <Route path='/' exact component={Home}/>
+          <Route path ='/results' component={Results}/>
         </Switch>
       </BrowserRouter>
     </Provider>,
