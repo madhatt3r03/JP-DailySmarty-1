692a981ed940eeefeb1712d04e4ae332211409cb
John Phillips <madhatter003@yahoo.com>
2019-05-29 12:33:33 -0600

    setup redux devtools and fixed recentPosts in the the postsReducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,20 +1,17 @@
-import { SET_RECENT_POSTS } from './../actions/types';
-import {
-    SET_RECENT_POSTS
-} from  '../actions/types';
-import { bindActionCreators } from 'redux';
+import { SET_RECENT_POSTS } from "../actions/types";
+import { bindActionCreators } from "redux";
 
 const INIT_STATE = {
-    posts: [],
-    recentPosts: []
-}
+	posts: [],
+	recentPosts: []
+};
 
 export default function(state = INIT_STATE, action) {
-    switch (action.type) {
-        case SET_RECENT_POSTS:
-
-        return [...state, recentPosts: action.payload]
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case SET_RECENT_POSTS:
+			const recentPosts = action.payload;
+			return { ...state, recentPosts: action.payload };
+		default:
+			return state;
+	}
 }
