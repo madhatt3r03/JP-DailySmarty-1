84f056bca07fc38af3273166b626daa4eabf4a56
John Phillips <madhatter003@yahoo.com>
2019-05-30 13:09:12 -0600

    put together resultsposts component

--- src/components/results.js
+++ src/components/results.js
@@ -1,22 +1,23 @@
-import { fetchPostsWithQuery } from './../actions/index';
 import React, { Component } from "react";
+
 import Logo from "./logo";
 import SearchBar from "./searchBar";
+import ResultsPosts from "./resultsPosts";
 
 import { connect } from "react-redux";
 import * as actions from "../actions";
 
 class Results extends Component {
-    handleSearchBarSubmit(query) {
-        this.props.fetchPostsWithQuery(query);
-    }
-    
+	handleSearchBarSubmit(query) {
+		this.props.fetchPostsWithQuery(query);
+	}
 
 	render() {
 		return (
 			<div>
 				<Logo size={55} />
 				<SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)} />
+                <ResultsPosts/>
 			</div>
 		);
 	}
---
+++ src/components/resultsPosts.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class ResultsPosts extends Component {
+	render() {
+		return (
+			<ul className="results-posts">
+				<div className="results-posts__wrapper">
+					<div className="results-posts__posts">Results Go Here</div>
+				</div>
+			</ul>
+		);
+	}
+}
+
+function mapStateToProps(state) {
+	return {
+		state
+	};
+}
+
+export default connect(mapStateToProps)(ResultsPosts);
