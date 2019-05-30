aeeeee0675fbe99f685c90d569b6bd7f90ce5dd7
John Phillips <madhatter003@yahoo.com>
2019-05-30 11:18:52 -0600

    modified logo component with inline styles and added searchBar component to results component

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,10 +2,16 @@ import React, { Component } from "react";
 
 class Logo extends Component {
 	render() {
+
+		const size = {
+			height: this.props.size ? this.props.size : 105,
+			width: this.props.size ? this.props.size : 105
+		}
+
 		return (
 			<div className="logo-main-wrapper">
 				<div className="logo-main">
-					<img alt="Photo goes here" src="/assets/DSlogo.png" />
+					<img style={size} alt="Photo goes here" src="/assets/DSlogo.png" />
 				</div>
 			</div>
 		);
