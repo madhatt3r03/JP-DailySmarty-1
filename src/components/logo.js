4c9353e97f4a28c129599e8e15fc0482fdd574fc
John Phillips <madhatter003@yahoo.com>
2019-05-31 22:30:49 -0600

    mapped state to props in resultsPosts Component and implemented home link in Logo Component

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 
+import { Link } from "react-router-dom";
+
 class Logo extends Component {
 	render() {
-
 		const size = {
 			height: this.props.size ? this.props.size : 105,
 			width: this.props.size ? this.props.size : 105
-		}
+		};
 
 		return (
 			<div className="logo-main-wrapper">
 				<div className="logo-main">
-					<img style={size} alt="Photo goes here" src="/assets/DSlogo.png" />
+					<Link to="/">
+						<img style={size} alt="Photo goes here" src="/assets/DSlogo.png" />
+					</Link>
 				</div>
 			</div>
 		);
