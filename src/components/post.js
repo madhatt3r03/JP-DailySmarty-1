38317e5d726713added3919e46f2570dcf2bf758
John Phillips <madhatter003@yahoo.com>
2019-06-02 13:46:58 -0600

    modified post component to render a result post and setup render links

--- src/components/post.js
+++ src/components/post.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Post extends Component {
 
@@ -6,21 +6,52 @@ class Post extends Component {
         let topics = this.props.associated_topics.map((topic, index) => {
             return <span className="post-topic" key={index}>{topic}</span>
         })
-
         return topics;
     }
 
+    renderLinks() {
+        let links = this.props.post_links.map((post_link, index) => {
+            return (
+                <div className="post-link" key={index}>
+                    <div className="post-link__box"></div>
+                    <div className="post-link__link">
+                        <a href={post_link.link_url}>Useful Link #{index + 1}</a>
+                    </div>
+                </div>
+            )
+        })
+        return links;
+    }
 
+    render() {
+        if(this.props.type == 'recent') {
+            return (
+                <li className="recent-post">
+                    <div className="recent-post__title">
+                        {this.props.title}
+                    </div>
+                    <div className="recent-post__topics">
+                        {this.renderTopics()}
+                    </div>
+                </li>
+            )
+        } else if(this.props.type == 'result') {
+            return (
+                <li className="result-post">
+                    <div className="result-post__topics">
+                        {this.renderTopics()}
+                    </div>
+                    <div className="result-post__title">
+                        {this.props.title}
+                    </div>
+                    <div className="result-post__links">
+                        {this.renderLinks()}
+                    </div>
+                </li>
+            )
+        }
 
-
-	render() {
-		return (
-			<li className="recent-post">
-				<div className="recent-post__title">{this.props.title}</div>
-                <div className="recent-topics">{this.renderTopics()}</div>
-			</li>
-		);
-	}
+    }
 }
 
 export default Post;
